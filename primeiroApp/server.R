d25a6ec97a43c34374e6f905d1b6a06d4694b9de
Gustavo Mourao <gustavohen1@gmail.com>
2020-07-13 21:44:03 -0300

    Implementando opcao automaticas

--- primeiroApp/server.R
+++ primeiroApp/server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 require(sp)
@@ -21,53 +12,59 @@ require(spatstat)
 require(tools)
 require(stringr)
 
-# Define server logic required to draw a histogram
-
-
+# Inicio do serve
 
 shinyServer(function(input, output, session) {
+  #   ### Quando o processo é automatizado desde o inicio, com o processo
+  #   ### automatico, a nuvem de pontos deve estar na pasta las e o arquivo csv
+  #   ### na pasta csv.  Os nomes foram padronizados nas etapas anteriores
   
+  #   #### Caso nao, deve-se definir o diretorio manualmente
+  
+  # Nome automatico ou manual
   
   output$transectoOutput <- renderUI({
-    
-    diretorio = input$diretorioInput
-    
-    inpD <- file_path_sans_ext(dir(paste0(diretorio,"\\las\\"),pattern='.las'))  # Arquivos na pasta las
-    inpE <- " "
-    inpD <- c(inpD, inpE)
-    
-    selectizeInput("transectoInput", "Transecto", choices = inpD,  selected = " ")
-    
+    if (input$autoProcess) {
+      diretorio = input$diretorioInput
+      
+      inpD <-
+        file_path_sans_ext(dir(paste0(diretorio, "\\las\\"), pattern = '.las'))  # Arquivos na pasta las
+      inpE <- " "
+      inpD <- c(inpD, inpE)
+      
+      selectizeInput("transectoInput",
+                     "Transecto",
+                     choices = inpD,
+                     selected = " ")
+    } else{
+      textInput("transectoInput", "Transecto", value = "Nome da Planilha")
+    }
   })
   
-  # botao para iniciar o processo   
   
-  observeEvent(input$n, {
-    label <- paste0("Simular ", input$n, " vezes")
-    updateActionButton(session, "go", label = label)
-  })
-  
-  # Atualizando o nome do shp
+  # Atualiza o nome do shp automaticamente
   
   observeEvent(input$transectoInput, {
-    
-    updateTextInput(session, "arquivoSHP", value = input$transectoInput )
+    updateTextInput(session, "arquivoSHP", value = input$transectoInput)
     
   })
   
   observeEvent(input$arquivoSHP, {
-    
-    updateTextInput(session, "arquivoSHP", value = input$arquivoSHP )
+    updateTextInput(session, "arquivoSHP", value = input$arquivoSHP)
     
   })
   
-  # Calculo da funcao 
+  
+  
+  # Calculo da funcao K de Ripley
   
   
   KdeRipley_ <- eventReactive(input$go, {
-    
-    
     diretorio <- input$diretorioInput
+    
+    
+    # padronizar variaveis
+    #------------
     
     inputT <- input$transectoInput
     
@@ -79,34 +76,71 @@ shinyServer(function(input, output, session) {
     
     # Carreganfo dados
     
-    # treeList1 <- read.table(paste0(diretorio,"\\csv\\",inputT,"tree_tile_0001_0001_treelist.csv"), header = T, sep = ",", dec = ".")
-    # treeList2 <- read.table(paste0(diretorio,"\\csv\\",inputT,"tree_tile_0001_0002_treelist.csv"), header = T, sep = ",", dec = ".")
-    treeList3 <- read.table(paste0(diretorio,"\\csv\\",inputT,"tree_tile_0002_0001_treelist.csv"), header = T, sep = ",", dec = ".")
-    # treeList4 <- read.table(paste0(diretorio,"\\csv\\",inputT,"tree_tile_0002_0002_treelist.csv"), header = T, sep = ",", dec = ".")
-    # 
-    # emergentTree <- rbind(treeList1, treeList2, treeList3, treeList4)
     
-    emergentTree <- treeList3
+    # Criar funcao para selecionar automaticamente ou manualmente arquivos
+    # Colocar o numero de arquivos que serão selecionados 1, 2, 3 ou 4 plhanilhas
+    # definir diretorio manual ou automatico
+    # definir sep = ",", dec = "."
+    #-----------------------------------------------------
+    
+    # Nome automatico com processos anteriores ou manual
+    
+    
+    if (input$autoProcess) {
+      emergentTree <-
+        read.table(
+          paste0(
+            diretorio,
+            "\\csv\\",
+            inputT,
+            "tree_tile_0002_0001_treelist.csv"
+          ),
+          header = T,
+          sep = ",",
+          dec = "."
+        )
+    } else{
+      emergentTree <-
+        read.table(
+          paste0(diretorio, inputT, ".csv"),
+          header = T,
+          sep = ",",
+          dec = "."
+        )
+    }
     
     PPP <- emergentTree
     
     # Filtrando percentil 95
     
-    PPP %<>% filter(Height > quantile(PPP$Height,percentil))
+    PPP %<>% filter(Height > quantile(PPP$Height, percentil))
     
     # Criando poligono com extenções
     
-    coordinates(PPP) = ~X+Y
+    # Definir oque serão eixos X e Y
+    #---------------------------------
+    
+    coordinates(PPP) = ~ X + Y
     
     # criando windows apartir de shp
     
-    S <- readShapePoly(paste0(diretorio,diretorioSHP,transectoI,".shp" ))
+    # Opcao de criar diretorio manualmente
+    # colocar a opcao clipoly para area sem shp
+    # colocar opcao manual com todos os ponto ou definir limite manualmente com pontos
+    #-------------------------------------
+    
+    S <-
+      readShapePoly(paste0(diretorio, diretorioSHP, transectoI, ".shp"))
     
     SP <- as(S, "SpatialPolygons")
     
     W <- as(SP, "owin")
     
     # criando pontos a partir de dados shp
+    
+    #---------------------------------------
+    # Opcao com click poly
+    #----------------------------------------
     
     # criando ppp
     
@@ -116,53 +150,108 @@ shinyServer(function(input, output, session) {
     
     # K test e K test envelopado
     
-    ke <- envelope(p,Kest, nsim = input$n)
+    ke <- envelope(p, Kest, nsim = input$n)
     
     ke
     
     # plot(ke, xlim = c(1400,1460), main=" ")
     
     
+    #---------------------------
+    # Criar opcao F (l)
+    #------------------------------
+    
+    
   })
+  
+  
+  # Plotando o grafico no painel
   
   output$KdeRipley <- renderPlot({
+    plo <-
+      plot(KdeRipley_(),
+           
+           # Opcao com nome do grafico
+           #--------------------------
+           
+           main = "Testando",
+           xlim = c(input$xInput[1], input$xInput[2]))
     
-    plo <- plot(KdeRipley_(), main = "Testando", xlim = c(input$xInput[1],input$xInput[2]))
-    
-    plo })
+    plo
+  })
   
   
-  # atualizando maximo e minimo X do grafico
+  # botao para iniciar o processo
+  
+  # Trocar input$n por nome input$nSimulacao
+  #----------------------------
+  
+  observeEvent(input$n, {
+    label <- paste0("Simular ", input$n, " vezes")
+    updateActionButton(session, "go", label = label)
+  })
+  
+  # atualizando limites superior e inferior horizontal do grafico + 1
   
   observeEvent(input$go, {
-    
-    updateSliderInput(session,"xInput", "Eixo Horizontal", min = 0, max = as.integer((max(KdeRipley_()$r))),
-                      value = c(0,as.integer((max(KdeRipley_()$r)))))
+    updateSliderInput(
+      session,
+      "xInput",
+      "Eixo Horizontal",
+      min = 0,
+      max = as.integer((max(KdeRipley_(
+        
+      )$r))),
+      value = c(0, as.integer((
+        max(KdeRipley_()$r + 1)
+      )))
+    )
     
   })
+  
+  # Intervalo superio e inferior do grafico
+  # trocar tag
+  #------------------------------
+  
+  # Intervalo horizontal do grafico
   
   observeEvent(input$infX, {
     nInfX = input$infX
     nSupX = input$supX
     
-    updateSliderInput(session,"xInput", value = c(nInfX,nSupX))
+    updateSliderInput(session, "xInput", value = c(nInfX, nSupX))
     
   })
   
   observeEvent(input$supX, {
-    
     nSupX = input$supX
     nInfX = input$infX
     
-    updateSliderInput(session,"xInput", value = c(nInfX,nSupX))
+    updateSliderInput(session, "xInput", value = c(nInfX, nSupX))
     
   })
   
   
+  # Botao para redefinir intervalos para maximo e minimo
+  
+  
+  #------------------
+  #trocar tags
+  #----------------
+  
   observeEvent(input$redLimites, {
-    
-    updateSliderInput(session,"xInput", "Eixo Horizontal", min = 0, max = as.integer((max(KdeRipley_()$r))),
-                      value = c(0,as.integer((max(KdeRipley_()$r)))))
+    updateSliderInput(
+      session,
+      "xInput",
+      "Eixo Horizontal",
+      min = 0,
+      max = as.integer((max(KdeRipley_(
+        
+      )$r))),
+      value = c(0, as.integer((
+        max(KdeRipley_()$r)
+      )))
+    )
   })
   
   
--- primeiroApp/ui.R
+++ primeiroApp/ui.R
@@ -1,18 +1,3 @@
-
-
-
-
-
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-
-
-
 library(shiny)
 
 library(shiny)
@@ -30,99 +15,118 @@ require(tools)
 require(stringr)
 
 
-# Define UI for application that draws a histogram
-
-
 #shiny::runApp(display.mode="showcase")
 
 
 
-
-
-
-
-shinyUI(fluidPage(
-  # Application title
-  
-  titlePanel("Testando APP"),
-  
-  "testando",
-  strong("Transectos"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      # Diretorio
-      
-      textInput(
-        "diretorioInput",
-        "Diretorio raiz",
-        value = "",
-        width = NULL,
-        placeholder = NULL
-      ),
-      
-      "Selecionar do diretorio raiz, como em C:/*/CSV",
-      br(),
-      br(),
-      "* = dieretorio",
-      
-      
-      # Selecionar transectos
-      
-      uiOutput("transectoOutput"),
-      
-      
-      
-      textInput(
-        "diretorioSHP",
-        "Diretorio shp",
-        value = "/qgis/finalizados/",
-        width = NULL,
-        placeholder = NULL
-      ),
-      
-      textInput(
-        "arquivoSHP",
-        "Arquivo shp",
-        value = "planilha",
-        width = NULL,
-        placeholder = NULL
-      ),
-      
-      # Percentil
-      
-      numericInput("percentil", "Filtro de Arvores (em decimais)", 0.95, width = "50%"),
-      
-      # numero de simulacoes
-      
-      numericInput("n", "Simulacoes", 3),
-      
-      # Executar processo
-      
-      actionButton("go", "Go!"),
-      
-      #  Maximo e minimo X do grafico
-      
-      sliderInput(
-        "xInput",
-        "Eixo Horizontal",
-        min = 0,
-        max = 100,
-        value = c(0, 100)
-      ),
-      
-      # inserir valores de limites superiores e inferiores
-      
-      numericInput("infX", "Inferior", 0, width = "50%"),
-      numericInput("supX", "Superior", 100, width = "50%"),
-      actionButton("redLimites", "Redefinir")
-      
-    ),
+shinyUI(
+  fluidPage(
     
-    mainPanel(plotOutput("KdeRipley"))
+    # Titulo da Aplicacao
+    
+    titlePanel("Fusion Emergent"),
+    
+    "Realizando tarefas para identificar arvores emergentes e ",
+    strong("padrao de distribuicao espacial"),
+    
+    # Layout da pagina
+    
+    sidebarLayout(
+      
+      # Paineis
+      
+      
+      sidebarPanel(
+        
+        # Difinir se o processo é automatico ou nao 
+        
+        strong("O processo foi automatizado?"),
+        
+        checkboxInput("autoProcess", "Sim"),
+        
+        # Box para definiri o Diretorio
+        
+        textInput(
+          "diretorioInput",
+          "Diretorio raiz",
+          value = "",
+          width = NULL,
+          placeholder = NULL
+        ),
+        
+        "Selecionar do diretorio raiz, como em C:/*/CSV",
+        br(),
+        br(),
+        "* = dieretorio",
+        
+        
+        # Selecionar transectos com identificacao automatica de nome
+        
+        uiOutput("transectoOutput"),
+        
+        
+        # Selecionar diretorio do shp
+        
+        textInput(
+          "diretorioSHP",
+          "Diretorio shp",
+          value = "/shp/",
+          width = NULL,
+          placeholder = NULL
+        ),
+        
+        # Inserir o nome do arquivo shp
+        
+        
+        textInput(
+          "arquivoSHP",
+          "Arquivo shp",
+          value = "planilha",
+          width = NULL,
+          placeholder = NULL
+        ),
+        
+        
+        # Dfinir qual percentil das arvores serao filtrados
+        
+        numericInput("percentil", "Filtro de Arvores (em decimais)", 0.95, width = "50%"),
+        
+        # numero de simulacoes para funcao k de Ripley
+        
+        numericInput("n", "Simulacoes", 3),
+        
+        # Botao para Executar processo
+        
+        actionButton("go", "Go!"),
+        
+        #  Limites superiores e inferiores horizontal do grafico com uma barra
+        
+        sliderInput(
+          "xInput",
+          "Eixo Horizontal",
+          min = 0,
+          max = 100,
+          value = c(0, 100)
+        ),
+        
+        # inserir valores de limites superiores e inferiores manualmente
+        
+        numericInput("infX", "Inferior", 0, width = "50%"),
+        numericInput("supX", "Superior", 100, width = "50%"),
+        actionButton("redLimites", "Redefinir")
+        
+      ),
+      
+      
+      
+      # Inicio do painel
+      
+      mainPanel(# Plotando o grafico para K de Ripley
+        
+        plotOutput("KdeRipley"))
+    )
   )
-))
+)
 # dsad
-# 
+#
 # asd
-
